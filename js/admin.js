ce72b27dc76a8fdd9fa6da73ad61ffc562ea4591
LouiseYLi <louiseli.van@gmail.com>
2025-03-16 10:00:19 -0700

    implemented admin check and started on admin page

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+function init() {
+    document.getElementById('start-btn').addEventListener('click', function() {
+        window.location.href = 'https://personamaker.netlify.app/questions'
+    })
+
+    // TODO: query for api calls left
+}
+
+init()
---
+++ js/isAdmin.js
@@ -0,0 +1,22 @@
+async function isAdmin() {
+    const req = {
+        method: "GET",
+        credentials: 'include'
+    }
+
+    try {
+        const res = await fetch("https://fortunedgalab.xyz/admin", req);
+
+        if (!res.ok) {
+            window.location.href = './login.html';
+            alert(`${res.status} ${res.statusText}: `)
+            return
+        }
+        console.log(res)
+    } catch (e) {
+        window.location.href = './login.html';
+        alert(`${res.status} ${res.statusText}: Failed authenticating`)
+    }
+}
+
+isAdmin()
