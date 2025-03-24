b449645eb0bd81b90bf55e8d211f02521a523ebf
LouiseYLi <louiseli.van@gmail.com>
2025-03-24 14:41:12 -0700

    moved user facing msgs of 403 page to js file

--- js/403.js
+++ js/403.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.title = TITLE_403
+    document.querySelector(".display-5.fw-bold").textContent = TITLE_403
+    document.querySelector(".lead.mb-4").textContent = DESCRIPTION_403
+    document.querySelector("#home-btn").textContent = HOME
+})
+
 document.getElementById('home-btn').addEventListener('click', (e) => {
     window.location.href = "./index.html"
 })
--- js/eng.js
+++ js/eng.js
@@ -1,1 +1,7 @@
 const URL = "https://fortunedgalab.xyz/"
+const HOME = "Home"
+
+
+// 403
+const TITLE_403 = "403 Not Authorised"
+const DESCRIPTION_403 = "You are not authorised to view this page."
