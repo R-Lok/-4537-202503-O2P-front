7fa3bd316875de6b258bbce139563b05c307bd5d
Ryan Lok <rlok.pc@gmail.com>
2025-03-27 10:52:25 -0700

    Add return statement to init()

--- js/admin.js
+++ js/admin.js
@@ -84,6 +84,7 @@ async function toggleBan(email, enabled) {
 async function init() {
     if (!(await isAdmin())) {
         window.location.href = './403.html'
+        return
     }
 
     document.body.style.display = 'block'
